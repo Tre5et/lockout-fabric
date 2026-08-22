30eb89ce42ff629e14812645e4ad3a068185d817
TreSet <hanneshekl@gmail.com>
2026-08-22 13:09:15 +0200

    feat: add groups and categories

--- src/main/java/me/marin/lockout/lockout/goal/config/GoalCategory.java
+++ src/main/java/me/marin/lockout/lockout/goal/config/GoalCategory.java
@@ -4,8 +4,24 @@ import lombok.Getter;
 
 public enum GoalCategory {
     TOOLS("Tools"),
-    OBTAIN("Obtain"),
-    RIDE("Ride");
+    MINING("Mining"),
+    DIMENSIONS("Dimensions"),
+    ARMOR("Armor"),
+    TAMING("Taming"),
+    BREEDING("Breeding"),
+    KILLING("Killing"),
+    OBTAINING_ITEMS("Obtaining Items"),
+    BREWING("Brewing"),
+    BIOMES("Biomes"),
+    EATING_DRINKING("Eating/Drinking"),
+    MISC_ACTIONS("Misc Actions"),
+    ADVANCEMENTS("Advancements"),
+    STATUS_EFFECTS("Status Effects"),
+    EXPERIENCE("Experience"),
+    RIDING("Riding"),
+    WORKSTATIONS("Workstations"),
+    DEATH_DAMAGE("Death/Damage"),
+    TEAM_GOALS("Team Goals");
 
     @Getter
     private final String name;
